a3f421e4eac832894d68e8f81eea55e37f0293fd
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2017-11-07 21:17:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 
 function addElementToBeginningOfArray(array,element){
 
-  var  array=[,"foo"]
+  var  array=["","foo"]
   array[1]= "foo"
   return array
 }
