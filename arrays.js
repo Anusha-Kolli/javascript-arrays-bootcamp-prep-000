dfbb9343ef7e345fa7935778cb9f9f63cbf664f5
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2017-11-07 21:39:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,5 +9,8 @@ function addElementToBeginningOfArray(array,element){
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
-
+}
+function addElementToEndOfArray(array, element){
+  array.push(element)
+  return array
 }
