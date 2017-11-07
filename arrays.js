a7917cf0a6c53978354f79146f5fd75ebc4efdeb
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2017-11-07 21:49:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array
 }
 function removeElementFromBeginningOfArray(array){
-  array.slice()
+  array.slice(1)
   return array
 }
